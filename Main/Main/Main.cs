cce11f6afe488a5225728c3cdc5e55c8fc45a874
시혁 박 <exon.indiv@gmail.com>
2020-08-02 20:46:09 +0900

    Add 주석

---
+++ Main/Main/Main.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        string key, value = string.Empty;
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            GetAje();
+            timer.Start();
+
+        }
+
+        private void submission_Click(object sender, EventArgs e)
+        {
+            Submit();
+        }
+
+        private void result_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Submit();
+            }
+        }
+
+        private void Submit()
+        {
+
+            if (result.Text == value) // 답이 같지 않으면?
+            {
+                GetAje();
+                result.Text = string.Empty;
+                msg.Text = "맞았습니다!";
+            }
+            else if (value.Contains("|")) // 답의 경우수가 여러개이면?
+            {
+                int isTrue = 0;
+                string[] v = value.Split('|');
+                for (int i = 0; i < v.Length; i++)
+                {
+                    if (v[i] == result.Text)
+                    {
+                        GetAje();
+                        result.Text = string.Empty;
+                        msg.Text = "맞았습니다!";
+                        isTrue = 1;
+                        break;
+                    }
+                }
+                if (isTrue == 0)
+                {
+                    result.Text = string.Empty;
+                    msg.Text = "틀렸습니다.";
+                }
+
+
+            }
+            else
+            {
+                result.Text = string.Empty;
+                msg.Text = "틀렸습니다.";
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void GetAje()
+        {
+            Data data = new Data();
+
+            data.Problem(ref key, ref value);
+
+            problem.Text = key;
+
+        }
+    }
+}
--- Main/Main/data.cs
+++ Main/Main/data.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace Main
 {
-    public class Data
+    public class Data 
     {
+        // 아재개그 모음
         public Dictionary<string, string> Ajegag = new Dictionary<string, string>();
 
 
@@ -16,6 +17,7 @@ namespace Main
             Reset();
         }
 
+        // 리셋 함수
         public void Reset()
         {
             Ajegag.Add("소금의 유통기한은 몇일일까?", "천일");
@@ -28,7 +30,7 @@ namespace Main
             Ajegag.Add("땅이 우는 소리는?", "흙흙");
             Ajegag.Add("전투기 조종사들이 제일 무서워하는 것은?", "대공포");
             Ajegag.Add("세발낙지가 탈모가 오면?", "한발낙지");
-            Ajegag.Add("토끼가 강한 이유는?", "깡과 총이 있어서");
+            Ajegag.Add("토끼가 강한 이유는? (~과~이 있어서)", "깡과 총이 있어서|깡과총이있어서|깡,총|총과 깡이 있어서|총과깡이있어서|총, 깡|깡, 총|총,깡|깡,총");
             Ajegag.Add("개가 재채기를 하면?", "개추");
             Ajegag.Add("흡혈귀들이 식탁에서 웃음이 끊이질 않는 이유는? (~~해서)", "피식해서");
             Ajegag.Add("스님이 택시를 타고 한 말은?", "절로가");
@@ -48,13 +50,10 @@ namespace Main
         {
             Random rand = new Random();
 
-            int num = rand.Next(0, Ajegag.Count);
+            int num = rand.Next(0, Ajegag.Count); // 랜덤으로 아재개그 하나 뽑기
             key = Ajegag.ElementAt(num).Key;
 
-
-
             value = Ajegag[key];
-
         }
     }
 }
